abf6b398fc07b7c501949263f94067ca3e300845
DEV REDDY <devreddy4444@gmail.com>
2024-07-22 13:42:29 +0530

    default host set to 0.0.0.0

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ import app from "./index.js";
 
 //Setting up the port
 const PORT = process.env.PORT || 3300;
+const host = process.env.HOST || "0.0.0.0";
 
 //Starting the server
-app.listen(PORT, () => {
+app.listen(PORT, host, () => {
   console.log(`Server is running on port ${PORT}`);
 });
